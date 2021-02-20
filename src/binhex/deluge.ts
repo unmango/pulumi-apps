6f05af82a43eba1576acb2bf7ae38cdf6638b72f
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-19 22:04:42 -0600

    The filthy seconds

--- src/binhex/deluge.ts
+++ src/binhex/deluge.ts
@@ -1,5 +1,19 @@
-import { ComponentResource } from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
+import { ComponentResourceOptions } from '@pulumi/pulumi';
+import { Input } from '@pulumi/pulumi/output';
+import { ContainerApp, ContainerAppArgs } from '../container-app';
 
-export class Deluge extends ComponentResource {
-    
+export class Deluge extends ContainerApp {
+
+  constructor(name: string, args: DelugeArgs, opts?: ComponentResourceOptions) {
+    super('deluge', name, {
+      image: '',
+      ...args,
+    }, opts);
+  }
+
+}
+
+export interface DelugeArgs extends ContainerAppArgs {
+  vpn: Input<string>;
 }
