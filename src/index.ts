4d8297276d27df691dd0b0b5b6e1774a178c17b8
UnstoppableMango <ecr.referee@hotmail.com>
2021-03-23 02:13:38 -0500

    feat: Add longhorn (#33)

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ export * from './helm-app';
 
 import * as binhex from './binhex';
 import * as bitnami from './bitnami';
+import * as rancher from './rancher';
 import * as util from './util';
 
 export {
   binhex,
   bitnami,
+  rancher,
   util,
 };
---
+++ src/rancher/index.ts
@@ -0,0 +1,1 @@
+export { newLonghorn, LonghornArgs } from './longhorn';
